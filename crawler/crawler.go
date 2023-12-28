42b4b95b348f04095e3fa45efd867f3998901cfd
Ankur Narkhede <ankurnarkhede@gmail.com>
2023-12-28 09:28:30 +0530

    GO fmt

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,193 +1,193 @@
 package crawler
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "net/url"
-    "sync"
-    "time"
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
 
-    "github.com/PuerkitoBio/goquery"
-    "github.com/chromedp/cdproto/cdp"
-    "github.com/chromedp/chromedp"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 )
 
 var wg sync.WaitGroup
 
 type crawlData struct {
-    sync.RWMutex
-    visited map[string]bool
-    result  []string
+	sync.RWMutex
+	visited map[string]bool
+	result  []string
 }
 
 func CrawlWebpage(rootURL string, maxDepth int, sameSite bool, loadDynamicContent bool) ([]string, error) {
-    var crawlSessionData = crawlData{visited: make(map[string]bool), result: []string{}}
-    fmt.Printf("CrawlWebpage: rootURL:: %v, maxDepth:: %v, sameSite:: %v, loadDynamicContent:: %v\n", rootURL, maxDepth, sameSite, loadDynamicContent)
+	var crawlSessionData = crawlData{visited: make(map[string]bool), result: []string{}}
+	fmt.Printf("CrawlWebpage: rootURL:: %v, maxDepth:: %v, sameSite:: %v, loadDynamicContent:: %v\n", rootURL, maxDepth, sameSite, loadDynamicContent)
 
-    // Add the initial URL to the results
-    crawlSessionData.Lock()
-    crawlSessionData.result = append(crawlSessionData.result, rootURL)
-    crawlSessionData.Unlock()
+	// Add the initial URL to the results
+	crawlSessionData.Lock()
+	crawlSessionData.result = append(crawlSessionData.result, rootURL)
+	crawlSessionData.Unlock()
 
-    wg.Add(1)
-    go crawl(rootURL, rootURL, 0, maxDepth, sameSite, loadDynamicContent, &crawlSessionData)
+	wg.Add(1)
+	go crawl(rootURL, rootURL, 0, maxDepth, sameSite, loadDynamicContent, &crawlSessionData)
 
-    c := make(chan struct{})
-    timeout := 1 * time.Minute
-    go func() {
-        fmt.Printf("CrawlWebpage: Waiting for active goroutines to finish.\n")
-        defer close(c)
-        wg.Wait()
-    }()
-    select {
-    case <-c:
-        fmt.Printf("CrawlWebpage: all goroutines finished executing.\n")
-    case <-time.After(timeout):
-        fmt.Printf("CrawlWebpage: timedout after %s. Returning the processed results.\n", timeout)
-    }
+	c := make(chan struct{})
+	timeout := 1 * time.Minute
+	go func() {
+		fmt.Printf("CrawlWebpage: Waiting for active goroutines to finish.\n")
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		fmt.Printf("CrawlWebpage: all goroutines finished executing.\n")
+	case <-time.After(timeout):
+		fmt.Printf("CrawlWebpage: timedout after %s. Returning the processed results.\n", timeout)
+	}
 
-    crawlSessionData.Lock()
-    crawlSessionData.result = removeDuplicates(crawlSessionData.result)
-    crawlSessionData.Unlock()
+	crawlSessionData.Lock()
+	crawlSessionData.result = removeDuplicates(crawlSessionData.result)
+	crawlSessionData.Unlock()
 
-    return crawlSessionData.result, nil
+	return crawlSessionData.result, nil
 }
 
 func crawl(url, rootURL string, depth, maxDepth int, sameSite, loadDynamicContent bool, crawlSessionData *crawlData) {
-    defer wg.Done()
+	defer wg.Done()
 
-    crawlSessionData.RLock()
-    urlVisited := crawlSessionData.visited[url]
-    crawlSessionData.RUnlock()
+	crawlSessionData.RLock()
+	urlVisited := crawlSessionData.visited[url]
+	crawlSessionData.RUnlock()
 
-    if depth > maxDepth || urlVisited {
-        return
-    }
+	if depth > maxDepth || urlVisited {
+		return
+	}
 
-    crawlSessionData.Lock()
-    crawlSessionData.result = append(crawlSessionData.result, url)
-    crawlSessionData.visited[url] = true
-    crawlSessionData.Unlock()
+	crawlSessionData.Lock()
+	crawlSessionData.result = append(crawlSessionData.result, url)
+	crawlSessionData.visited[url] = true
+	crawlSessionData.Unlock()
 
-    fmt.Printf("CrawlWebpage: Crawling %s (depth %d)\n", url, depth)
+	fmt.Printf("CrawlWebpage: Crawling %s (depth %d)\n", url, depth)
 
-    if loadDynamicContent {
-        processDynamicContent(url, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
-    } else {
-        processStaticContent(url, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
-    }
+	if loadDynamicContent {
+		processDynamicContent(url, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+	} else {
+		processStaticContent(url, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+	}
 }
 
 func processDynamicContent(url, rootURL string, depth, maxDepth int, sameSite, loadDynamicContent bool, crawlSessionData *crawlData) {
-    var nodes []*cdp.Node
+	var nodes []*cdp.Node
 
-    ctx, cancel := chromedp.NewContext(context.Background())
-    defer cancel()
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
 
-    err := chromedp.Run(ctx,
-        chromedp.Navigate(url),
-    )
-    if err != nil {
-        fmt.Printf("CrawlWebpage: Error in navigating to URL:: %v, err:: %v\n", url, err)
-        return
-    }
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+	)
+	if err != nil {
+		fmt.Printf("CrawlWebpage: Error in navigating to URL:: %v, err:: %v\n", url, err)
+		return
+	}
 
-    // @todo: Add a delay here if required if the dynamic content isn't loaded
+	// @todo: Add a delay here if required if the dynamic content isn't loaded
 
-    err = chromedp.Run(ctx,
-        chromedp.Nodes(`a[href]`, &nodes, chromedp.ByQueryAll),
-    )
-    if err != nil {
-        fmt.Printf("CrawlWebpage: Error in getting links for URL:: %v, err:: %v\n", url, err)
-        return
-    }
+	err = chromedp.Run(ctx,
+		chromedp.Nodes(`a[href]`, &nodes, chromedp.ByQueryAll),
+	)
+	if err != nil {
+		fmt.Printf("CrawlWebpage: Error in getting links for URL:: %v, err:: %v\n", url, err)
+		return
+	}
 
-    processLinks(url, rootURL, nodes, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+	processLinks(url, rootURL, nodes, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
 }
 
 func processStaticContent(url, rootURL string, depth, maxDepth int, sameSite, loadDynamicContent bool, crawlSessionData *crawlData) {
-    doc, err := fetchHTML(url)
-    if err != nil {
-        fmt.Printf("Error fetching %s: %v\n", url, err)
-        return
-    }
+	doc, err := fetchHTML(url)
+	if err != nil {
+		fmt.Printf("Error fetching %s: %v\n", url, err)
+		return
+	}
 
-    doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
-        link, _ := s.Attr("href")
-        if link != "" {
-            absURL := resolveURL(url, link)
-            processLink(absURL, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
-        }
-    })
+	doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("href")
+		if link != "" {
+			absURL := resolveURL(url, link)
+			processLink(absURL, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+		}
+	})
 }
 
 func processLink(absURL, rootURL string, depth, maxDepth int, sameSite, loadDynamicContent bool, crawlSessionData *crawlData) {
-    crawlSessionData.RLock()
-    urlVisited := crawlSessionData.visited[absURL]
-    crawlSessionData.RUnlock()
+	crawlSessionData.RLock()
+	urlVisited := crawlSessionData.visited[absURL]
+	crawlSessionData.RUnlock()
 
-    if !urlVisited && (!sameSite || isSameSite(rootURL, absURL)) {
-        crawlSessionData.Lock()
-        crawlSessionData.result = append(crawlSessionData.result, absURL)
-        crawlSessionData.Unlock()
+	if !urlVisited && (!sameSite || isSameSite(rootURL, absURL)) {
+		crawlSessionData.Lock()
+		crawlSessionData.result = append(crawlSessionData.result, absURL)
+		crawlSessionData.Unlock()
 
-        wg.Add(1)
-        go crawl(absURL, rootURL, depth+1, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
-    }
+		wg.Add(1)
+		go crawl(absURL, rootURL, depth+1, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+	}
 }
 
 func processLinks(url, rootURL string, nodes []*cdp.Node, depth, maxDepth int, sameSite, loadDynamicContent bool, crawlSessionData *crawlData) {
-    for _, node := range nodes {
-        link := node.AttributeValue("href")
-        if link != "" {
-            absURL := resolveURL(url, link)
-            processLink(absURL, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
-        }
-    }
+	for _, node := range nodes {
+		link := node.AttributeValue("href")
+		if link != "" {
+			absURL := resolveURL(url, link)
+			processLink(absURL, rootURL, depth, maxDepth, sameSite, loadDynamicContent, crawlSessionData)
+		}
+	}
 }
 
 func fetchHTML(url string) (*goquery.Document, error) {
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-    if err != nil {
-        return nil, err
-    }
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
+	}
 
-    doc, err := goquery.NewDocumentFromReader(resp.Body)
-    if err != nil {
-        return nil, err
-    }
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-    return doc, nil
+	return doc, nil
 }
 
 func isSameSite(rootURL, absURL string) bool {
-    root, _ := url.Parse(rootURL)
-    abs, _ := url.Parse(absURL)
-    return root.Host == abs.Host
+	root, _ := url.Parse(rootURL)
+	abs, _ := url.Parse(absURL)
+	return root.Host == abs.Host
 }
 
 func resolveURL(baseURL, link string) string {
-    base, _ := url.Parse(baseURL)
-    rel, _ := url.Parse(link)
+	base, _ := url.Parse(baseURL)
+	rel, _ := url.Parse(link)
 
-    if rel.IsAbs() {
-        return rel.String()
-    }
+	if rel.IsAbs() {
+		return rel.String()
+	}
 
-    // Otherwise, resolve it relative to the base URL
-    return base.ResolveReference(rel).String()
+	// Otherwise, resolve it relative to the base URL
+	return base.ResolveReference(rel).String()
 }
